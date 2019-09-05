a2c865f6f063511eda0fbd5ca896971cf226d459
Roshanak Agahi <rosh.agahi@gmail.com>
2019-09-05 19:33:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+  array[2], array [1] = array [1], array [2]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
 
